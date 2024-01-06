f052888c95c16eff584abd19258f73e58fc0bba7
kumastry <chma19063@g.nihon-u.ac.jp>
2024-01-06 17:44:28 +0900

    フィルター機能を別関数に移した

--- src/components/problemset.tsx
+++ src/components/problemset.tsx
@@ -1,6 +1,7 @@
 import Card from "./card";
 import type { ProblemSetPropsType } from "./../types/props";
 import { memo } from "react";
+import { filterByColor, filterByDiff } from "./../logic/filterCard";
 
 const ProblemSet = memo(
   ({
@@ -9,47 +10,11 @@ const ProblemSet = memo(
     diffFilter,
     subFilter,
   }: ProblemSetPropsType): React.JSX.Element => {
-    const filteredBycolororProblems = problems.filter((element) => {
-      let color = "all";
-      if (element.diff < 400) {
-        color = "gray";
-      } else if (element.diff < 800) {
-        color = "brawn";
-      } else if (element.diff < 1200) {
-        color = "green";
-      } else if (element.diff < 1600) {
-        color = "cyan";
-      } else if (element.diff < 2000) {
-        color = "blue";
-      } else if (element.diff < 2400) {
-        color = "yellow";
-      } else if (element.diff < 2800) {
-        color = "orange";
-      } else {
-        color = "red";
-      }
+    // フィルターが増えることを考慮してリファクタリングすべき
+    const filteredProblems = problems
+      .filter((element) => filterByColor(element, diffFilter))
+      .filter((element) => filterByDiff(element, subFilter));
 
-      if (diffFilter === "all") {
-        return true;
-      }
-      return diffFilter === color;
-    });
-
-    const filteredProblems = filteredBycolororProblems.filter((element) => {
-      let sub = "all";
-      if (element.sub === "AC") {
-        sub = "AC";
-      } else if (element.sub === "WA") {
-        sub = "WA";
-      } else if (element.sub === "nosub") {
-        sub = "nosub";
-      }
-
-      if (subFilter === "all") {
-        return true;
-      }
-      return subFilter === sub;
-    });
     console.log("problem");
     return (
       <ul className="is-vertical-center">
---
+++ src/logic/filterCard.tsx
@@ -0,0 +1,53 @@
+import type {
+  SubmissionType,
+  DifficultyType,
+  ProblemType,
+} from "./../types/base";
+
+export const filterByColor = (
+  element: ProblemType,
+  diffFilter: DifficultyType,
+) => {
+  let color = "all";
+  if (element.diff < 400) {
+    color = "gray";
+  } else if (element.diff < 800) {
+    color = "brawn";
+  } else if (element.diff < 1200) {
+    color = "green";
+  } else if (element.diff < 1600) {
+    color = "cyan";
+  } else if (element.diff < 2000) {
+    color = "blue";
+  } else if (element.diff < 2400) {
+    color = "yellow";
+  } else if (element.diff < 2800) {
+    color = "orange";
+  } else {
+    color = "red";
+  }
+
+  if (diffFilter === "all") {
+    return true;
+  }
+  return diffFilter === color;
+};
+
+export const filterByDiff = (
+  element: ProblemType,
+  subFilter: SubmissionType,
+) => {
+  let sub = "all";
+  if (element.sub === "AC") {
+    sub = "AC";
+  } else if (element.sub === "nosub") {
+    sub = "nosub";
+  } else {
+    sub = "WA";
+  }
+
+  if (subFilter === "all") {
+    return true;
+  }
+  return subFilter === sub;
+};
